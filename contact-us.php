0b2fcabae6a735fc0478d218c9b3f9985caea6f8
Vigneshrsn <vigneshrsn5@gmail.com>
2021-05-17 15:35:25 +0530

    corrected self-publishing page

--- contact-us.php
+++ contact-us.php
@@ -39,21 +39,20 @@
         <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
             <a href="index.php" class="logo d-flex align-items-center">
                 <img src="assets/img/whdph_logo.png" />
-    
+
             </a>
 
             <nav id="navbar" class="navbar">
                 <ul>
                     <li>
-                        <a class="nav-link scrollto" href="self-publishing.html">Book Publishing</a
-              >
-            </li>
-            <li>
-              <a class="nav-link scrollto" href="packages.html">Packages</a>
+                        <a class="nav-link scrollto" href="self-publishing.html">Book Publishing</a>
+                    </li>
+                    <li>
+                        <a class="nav-link scrollto" href="packages.html">Packages</a>
                     </li>
                     <li>
                         <a class="nav-link scrollto" href="free-publish.php">Quick Publish
-              </a>
+                        </a>
                     </li>
                     <li><a href="publishing-process.html">Publishing steps</a></li>
                     <li>
@@ -90,89 +89,95 @@
 
 
 
-                    <!-- ======= Contact Section ======= -->
-                    <section id="contact" class="contact">
-                        <div class="container" data-aos="fade-up">
-                            <header class="section-header">
-                                <h2>Contact</h2>
-                                <div class="text-center">
-            <h6 class="underline" style="color: #6e6e6e; font-size: 25px;">
-                <strong>CONTACT US</strong><br />
-            </h6>
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+        <div class="container" data-aos="fade-up">
+            <header class="section-header">
+                <h2>Contact</h2>
+                <div class="text-center">
+                    <h6 class="underline" style="color: #6e6e6e; font-size: 25px;">
+                        <strong>CONTACT US</strong><br />
+                    </h6>
 
-        </div>
-                            </header>
-    
-                            <div class="row gy-4">
-                                <div class="col-lg-6">
-                                    <div class="row gy-4">
-                                        <div class="col-md-6">
-                                            <div class="info-box">
-                                                <i class="bi bi-geo-alt"></i>
-                                                <h3>Address</h3>
-                                                <p>A108 Adam Street,<br />New York, NY 535022</p>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="info-box">
-                                                <i class="bi bi-telephone"></i>
-                                                <h3>Call Us</h3>
-                                                <p>+1 5589 55488 55<br />+1 6678 254445 41</p>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="info-box">
-                                                <i class="bi bi-envelope"></i>
-                                                <h3>Email Us</h3>
-                                                <p>info@example.com<br />contact@example.com</p>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="info-box">
-                                                <i class="bi bi-clock"></i>
-                                                <h3>Open Hours</h3>
-                                                <p>Monday - Friday<br />9:00AM - 05:00PM</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-    
-                                <div class="col-lg-6">
-                                    <form action="" method="post" enctype="multipart/form-data" class="php-email-form">
-                                        <div class="row gy-4">
-                                            <div class="col-md-6">
-                                                <input type="text" name="name" class="form-control" value="<?php echo !empty($postData['name'])?$postData['name']:''; ?>" placeholder="Your Name" required />
-                                            </div>
-    
-                                            <div class="col-md-6">
-                                                <input type="email" class="form-control" name="email" value="<?php echo !empty($postData['email'])?$postData['email']:''; ?>" placeholder="Your Email" required />
-                                            </div>
-    
-                                            <div class="col-md-12">
-                                                <input type="text" class="form-control" name="subject" value="<?php echo !empty($postData['subject'])?$postData['subject']:''; ?>" placeholder="Subject" required />
-                                            </div>
-    
-                                            <div class="col-md-12">
-                                                <textarea class="form-control" name="message" value="<?php echo !empty($postData['message'])?$postData['message']:''; ?>" rows="6" placeholder="Message" required></textarea>
-                                            </div>
-    
-                                            <div class="col-md-12 text-center">
-                                                <div class="loading">Loading</div>
-                                                <div class="error-message"></div>
-                                                <div class="sent-message">
-                                                    Your message has been sent. Thank you!
-                                                </div>
-    
-                                                <input type="submit" name="submit" class="btn btn-primary submit-now" value="SUBMIT">
-                                                <!-- <button type="submit">Send Message</button> -->
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
+                </div>
+            </header>
+
+            <div class="row gy-4">
+                <div class="col-lg-6">
+                    <div class="row gy-4">
+                        <div class="col-md-6">
+                            <div class="info-box">
+                                <i class="bi bi-geo-alt"></i>
+                                <h3>Address</h3>
+                                <p>A108 Adam Street,<br />New York, NY 535022</p>
                             </div>
                         </div>
-                    </section>
-                    <!-- End Contact Section -->
+                        <div class="col-md-6">
+                            <div class="info-box">
+                                <i class="bi bi-telephone"></i>
+                                <h3>Call Us</h3>
+                                <p>+1 5589 55488 55<br />+1 6678 254445 41</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="info-box">
+                                <i class="bi bi-envelope"></i>
+                                <h3>Email Us</h3>
+                                <p>info@example.com<br />contact@example.com</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="info-box">
+                                <i class="bi bi-clock"></i>
+                                <h3>Open Hours</h3>
+                                <p>Monday - Friday<br />9:00AM - 05:00PM</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <form action="" method="post" enctype="multipart/form-data" class="php-email-form">
+                        <div class="row gy-4">
+                            <div class="col-md-6">
+                                <input type="text" name="name" class="form-control" value="<?php echo !empty($postData['name']) ? $postData['name'] : ''; ?>" placeholder="Your Name" required />
+                            </div>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" value="<?php echo !empty($postData['email']) ? $postData['email'] : ''; ?>" placeholder="Your Email" required />
+                            </div>
+                            <div class="col-md-6 ">
+                                    <input type="email " class="form-control " name="email " placeholder="Your Email " required />
+                                </div>
+                                <div class="col-md-6 ">
+                                    <input type="text " name="Country" class="form-control " placeholder="Country" required />
+                                </div>
+
+                            <div class="col-md-12">
+                                <input type="text" class="form-control" name="subject" value="<?php echo !empty($postData['subject']) ? $postData['subject'] : ''; ?>" placeholder="Subject" required />
+                            </div>
+
+                            <div class="col-md-12">
+                                <textarea class="form-control" name="message" value="<?php echo !empty($postData['message']) ? $postData['message'] : ''; ?>" rows="6" placeholder="Message" required></textarea>
+                            </div>
+
+                            <div class="col-md-12 text-center">
+                                <div class="loading">Loading</div>
+                                <div class="error-message"></div>
+                                <div class="sent-message">
+                                    Your message has been sent. Thank you!
+                                </div>
+
+                                <input type="submit" name="submit" class="btn btn-primary submit-now"style="background-color: #ffffff;border-color: #4154f1; color:#356aac; border-radius:50px; font-weight:600;" onMouseOver="this.style.color='white';this.style.backgroundColor='#356aac'" onMouseOut="this.style.color='#356aac';this.style.backgroundColor='white'" value="SUBMIT">
+                                <!-- <button type="submit">Send Message</button> -->
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Contact Section -->
 
     <!-- address -->
     <div class="container">
@@ -215,52 +220,49 @@
                     <small>&copy; Copyright WHDPH. All Rights Reserved.</small>
                     <p><a href="#" data-toggle="modal" data-target="#privacy">Privacy</a> | <a href="#" data-toggle="modal" data-target="#terms">Terms of Use</a></p>
 
-          <!-- Privacy Modal end --> 
-          
-          <!-- Terms of Use Modal -->
-          <div class="modal fade" id="terms" tabindex="-1" role="dialog" aria-labelledby="myModalLabel-terms" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                  <h4 class="modal-title" id="myModalLabel-terms">Terms of Use</h4>
+                    <!-- Privacy Modal end -->
+
+                    <!-- Terms of Use Modal -->
+                    <div class="modal fade" id="terms" tabindex="-1" role="dialog" aria-labelledby="myModalLabel-terms" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                    <h4 class="modal-title" id="myModalLabel-terms">Terms of Use</h4>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Terms of Use Modal end -->
+
                 </div>
-                
-              </div>
             </div>
-          </div>
-          <!-- Terms of Use Modal end --> 
-          
         </div>
-      </div>
-    </div>
-  </footer>
-  <!--site-footer end-->
+    </footer>
+    <!--site-footer end-->
 
-<a
-  href="#"
-  class="back-to-top d-flex align-items-center justify-content-center"
-  ><i class="bi bi-arrow-up-short"></i
-></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-<!-- Vendor JS Files -->
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
-<script src="assets/vendor/aos/aos.js"></script>
-<!-- <script src="assets/vendor/php-email-form/validate.js"></script> -->
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="assets/vendor/purecounter/purecounter.js"></script>
-<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <!-- <script src="assets/vendor/php-email-form/validate.js"></script> -->
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
 
-<!-- bootstrap js cdn -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+    <!-- bootstrap js cdn -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
 
-<!-- Template Main JS File -->
-<script src="assets/js/main.js"></script>
-<!-- footer -->
-<script src="footerfiles/js/scripts.min.js"></script> 
-    <script src="footerfiles/js/jquery.ajaxchimp.min.js"></script> 
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <!-- footer -->
+    <script src="footerfiles/js/scripts.min.js"></script>
+    <script src="footerfiles/js/jquery.ajaxchimp.min.js"></script>
     <script src="footerfiles/js/main.js"></script>
 </body>
+
 </html>
